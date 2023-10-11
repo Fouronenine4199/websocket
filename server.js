1409398d1348cd8d02c3da1b39d1a53e476454d8
Fouronenine4199 <131957340+Fouronenine4199@users.noreply.github.com>
2023-10-11 21:18:44 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,18 @@
+const WebSocket = require('ws');
+const http = require('http'); // Import the http module
+const server = http.createServer((req, res) => {
+  // ... Your HTTP server code
+});
+
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+  ws.send("That is the vibe")
+  // WebSocket server logic here
+});
+
+// Start the server and bind to the appropriate host and port.
+server.listen(5050, '52.49.171.158', () => {
+  console.log('Server is listening on port 5050');
+});
