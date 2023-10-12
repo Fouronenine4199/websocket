578c1d1e80a93a2eef67c8c38a2ae42df80b7079
Fouronenine4199 <131957340+Fouronenine4199@users.noreply.github.com>
2023-10-12 21:57:41 +0100

    Update server.js

--- server.js
+++ server.js
@@ -17,12 +17,40 @@
 //   console.log('Server is listening on port 5050');
 // });
 
-var http = require('http');
-var server = http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    var message = 'It works!\n',
-        version = 'NodeJS ' + process.versions.node + '\n',
-        response = [message, version].join('\n');
-    res.end(response);
+// var http = require('http');
+// var server = http.createServer(function(req, res) {
+//     res.writeHead(200, {'Content-Type': 'text/plain'});
+//     var message = 'It works!\n',
+//         version = 'NodeJS ' + process.versions.node + '\n',
+//         response = [message, version].join('\n');
+//     res.end(response);
+// });
+// server.listen();
+
+
+
+const WebSocket = require('ws');
+
+const PORT = 8091
+
+const wss = new WebSocket.Server({ port: PORT }); // Create a WebSocket server on port 8080
+
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+
+  // Send a welcome message to the client
+  ws.send('Welcome to the WebSocket server!');
+
+  // Listen for messages from the client
+  ws.on('message', (message) => {
+    console.log(`Received: ${message}`);
+    // You can process the message here and send a response back if needed
+    ws.send(`You sent: ${message}`);
+  });
+
+  // Handle connection close
+  ws.on('close', () => {
+    console.log('Client disconnected');
+  });
 });
-server.listen();
+
